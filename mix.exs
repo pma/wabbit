bdfebf48385301a38e3bf3118a19577091825729
Paulo Almeida <paulo.almeida@timeboxed.co.ao>
2017-10-02 09:27:57 +0100

    Prepare for hex.pm

--- mix.exs
+++ mix.exs
@@ -7,6 +7,9 @@ defmodule Wabbit.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
+     source_url: "https://github.com/pma/wabbit",
      deps: deps()]
   end
 
@@ -19,6 +22,20 @@ defmodule Wabbit.Mixfile do
     [{:gen_stage, "~> 0.12"},
      {:connection, "~> 1.0"},
      {:amqp_client, "~> 3.6"},
-     {:rabbit_common, "~> 3.6"}]
+     {:rabbit_common, "~> 3.6"},
+     {:ex_doc, "~> 0.17", only: :dev}]
+  end
+
+  defp description do
+    """
+    GenStage based interface for RabbitMQ
+    """
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README.md", "LICENSE"],
+     maintainers: ["Paulo Almeida"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/pma/wabbit"}]
   end
 end
