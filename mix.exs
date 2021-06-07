2076fc6e459073497ebd4f6f1051f85d26dc5267
Wesley de Bruijn <wesley.dbr@live.nl>
2021-06-07 17:12:09 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -2,28 +2,34 @@ defmodule Wabbit.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :wabbit,
-     version: "0.4.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: description(),
-     package: package(),
-     source_url: "https://github.com/pma/wabbit",
-     deps: deps()]
+    [
+      app: :wabbit,
+      version: "0.5.0",
+      elixir: "~> 1.12",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/pma/wabbit",
+      deps: deps()
+    ]
   end
 
   def application do
-    [applications: [:logger, :asn1, :public_key, :ssl, :amqp_client, :connection, :gen_stage],
-     mod: {Wabbit, []}]
+    [
+      applications: [:logger, :asn1, :public_key, :ssl, :amqp_client, :connection, :gen_stage],
+      mod: {Wabbit, []}
+    ]
   end
 
   defp deps do
-    [{:gen_stage, "~> 1.0"},
-     {:connection, "~> 1.0"},
-     {:amqp_client, "~> 3.6"},
-     {:rabbit_common, "~> 3.6"},
-     {:ex_doc, "~> 0.17", only: :dev}]
+    [
+      {:gen_stage, "~> 1.1"},
+      {:connection, "~> 1.0"},
+      {:amqp_client, "~> 3.8"},
+      {:rabbit_common, "~> 3.8"},
+      {:ex_doc, "~> 0.17", only: :dev}
+    ]
   end
 
   defp description do
@@ -33,9 +39,11 @@ defmodule Wabbit.Mixfile do
   end
 
   defp package do
-    [files: ["lib", "mix.exs", "README.md", "LICENSE"],
-     maintainers: ["Paulo Almeida"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/pma/wabbit"}]
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Paulo Almeida"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/pma/wabbit"}
+    ]
   end
 end
